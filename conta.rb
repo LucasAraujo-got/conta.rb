de849248e7d3ae66da4762bc07069d6fa923f3aa
Lucas <lucas0714araujo@gmail.com>
2023-02-16 20:53:56 -0300

    alterações

--- conta.rb
+++ conta.rb
@@ -1,10 +1,27 @@
 class Conta
-    attr_reader :numero, :titular
-    attr_accessor :saldo
+        attr_reader :numero, :titular
+        attr_accessor :saldo
 
     def initialize(numero, titular, saldo)
         @numero = numero
         @titular = titular
         @saldo = saldo
+    end
+
+    def sacar(valor)
+        if saldo >= valor
+        self.saldo -= valor
+        else
+        puts "Não foi possivel executar o saque."
+        end
+    end
+
+    def depositar(valor)
+        self.saldo += valor
+    end
+
+    def transferir(conta_destino, valor)
+        sacar(valor)
+        conta_destino.depositar(valor)
     end
 end
